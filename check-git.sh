bc9a6a587567b7fdc24bd188559a3fa152dddb7a
RunAtTekky <varunrawat343@gmail.com>
2026-01-29 10:45:06 +0530

    add: script to check if contents of .zip files are git tracked

---
+++ check-git.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+CURR_DIR=$(pwd)
+
+# Find all the zip files
+    # File by file
+    # Unzip the file to res folder
+    # Check if the file contains .git folder
+    # If yes, print the name of the file
+    # Delete the res folder
+
+TARGET_FOLDER=".git"
+DEST_FOLDER="res"
+
+ALL_FILES=$(find . -name '*.zip')
+
+CNT=$(find . -name '*.zip' | wc -l)
+echo "Total zip files $CNT"
+
+count_commits() {
+    echo "Commit count:"
+    cd "$DEST_FOLDER"
+
+    git_folder=$(find . -name "$TARGET_FOLDER" -not -path '*__MACOS*')
+    cd "$git_folder"
+
+    cat logs/HEAD | wc -l
+
+    cd "$CURR_DIR"
+}
+
+echo "$ALL_FILES" | while read line
+do
+    # echo "---"
+    # echo "Reading: $line"
+    cd "$CURR_DIR"
+    rm -rf "$DEST_FOLDER"
+    unzip "$line" -d "$DEST_FOLDER" &>/dev/null
+
+    git_folders=$(find "$DEST_FOLDER" -name "$TARGET_FOLDER" | wc -l)
+    if [[ $git_folders -ne 0 ]]; then
+        echo "$line has git folder"
+        count_commits
+        echo "---"
+    fi
+
+    rm -rf "$DEST_FOLDER"
+    # echo "---"
+done
+
