2ba961f9b5a694fc5f82770883bbd5a659ce4a48
RunAtTekky <varunrawat343@gmail.com>
2026-02-05 23:33:20 +0530

    check-env: check files for API keys

--- check-env.sh
+++ check-env.sh
@@ -3,13 +3,14 @@
 CURR_DIR=$(pwd)
 
 TARGET=".env"
+PATTERN="os.environ"
 
 ALL_FILES=$(find . -name '*.zip')
 
 ZIP_CNT=$(find . -name '*.zip' | wc -l)
 echo "Total zip files $ZIP_CNT"
 
-UNZIP_LOCATION="/tmp/ml-unzipped"
+UNZIP_LOCATION="/tmp/day-2/ml-unzipped"
 mkdir -p "$UNZIP_LOCATION"
 
 counter=0
@@ -26,6 +27,16 @@ do
     if [[ $cnt -ne 0 ]]; then
         echo "$file"
         find "$DEST_FOLDER" -name "$TARGET" | xargs -I _ cat _
+        echo -e "\n---\n"
+        ((counter++))
+        # echo "$counter"
+    fi
+
+    cnt=$(grep -h -r --exclude-dir='*venv*' "$PATTERN" "$DEST_FOLDER" | grep -h -E "^.*=.*\'.{35}.*\'.*$" | wc -l)
+
+    if [[ $cnt -ne 0 ]]; then
+        echo "$file"
+        grep -h -r --exclude-dir='*venv*' "$PATTERN" "$DEST_FOLDER" | grep -h -E "^.*=.*\'.{35}.*\'.*$"
         echo -e "\n---\n"
         ((counter++))
         # echo "$counter"
