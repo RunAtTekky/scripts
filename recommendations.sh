2bbfffe6c01b3763c3144dbe0d252c1a55a7bf06
RunAtTekky <varunrawat343@gmail.com>
2026-09-21 19:31:26 +0530

    Add: logs

--- recommendations.sh
+++ recommendations.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -eoux pipefail
+set -eou pipefail
 
 # COLOUR CODES
 RED=$'\e[31m'
@@ -37,68 +37,81 @@ log_error() {
   echo -e "${RED}[INFO] $(get_current_date_and_time) - $1${RESET}"
 }
 
-log_info "Info"
-log_debug "Debug"
-log_error "Error"
-log_warn "Warn"
+# # For checking
+# log_info "Info"
+# log_debug "Debug"
+# log_error "Error"
+# log_warn "Warn"
 
-# # These are the files which I will be using
-# BASE_DIR="/tmp/recommendation-test"
+# These are the files which I will be using
+BASE_DIR="/tmp/recommendation-test"
 
-# VECTOR_DB_RESPONSE="$BASE_DIR/vector-db-response.json"
-# IN_VECTOR_DB="$BASE_DIR/in-vector-db.json"
+VECTOR_DB_RESPONSE="$BASE_DIR/vector-db-response.json"
+IN_VECTOR_DB="$BASE_DIR/in-vector-db.json"
 
-# VECTORID_TEMPLATENAME_IN_VECTOR_DB="$BASE_DIR/vectorId-templateName-in-vector-db.txt"
-# IN_DB="$BASE_DIR/in-db.json"
-# TEMPLATENAME_IN_DB="$BASE_DIR/templateName-in-db.txt"
-# TEMPLATENAME_IN_VECTOR_DB="$BASE_DIR/templateName-in-vector-db.txt"
+VECTORID_TEMPLATENAME_IN_VECTOR_DB="$BASE_DIR/vectorId-templateName-in-vector-db.txt"
+IN_DB="$BASE_DIR/in-db.json"
+TEMPLATENAME_IN_DB="$BASE_DIR/templateName-in-db.txt"
+TEMPLATENAME_IN_VECTOR_DB="$BASE_DIR/templateName-in-vector-db.txt"
 
-# COMMON_TEMPLATENAME="$BASE_DIR/result-common-templateName.json"
-# ONLY_IN_VECTOR_DB_TEMPLATENAME="$BASE_DIR/result-only-in-vector-db-templateName.json"
+COMMON_TEMPLATENAME="$BASE_DIR/result-common-templateName.json"
+ONLY_IN_VECTOR_DB_TEMPLATENAME="$BASE_DIR/result-only-in-vector-db-templateName.json"
 
-# VECTOR_ID_OF_COMMON_TEMPLATE_NAME="$BASE_DIR/vector-id-of-common-template-name.json"
-# VECTOR_ID_OF_UNIQUE_TEMPLATE_NAME="$BASE_DIR/vector-id-of-unique-template-name.json"
-# COMMON_FILE="$BASE_DIR/common.json"
-# UNIQUE_FILE="$BASE_DIR/unique.json"
-# RESULT_COMMON="$BASE_DIR/res-common.json"
-# RESULT_UNIQUE="$BASE_DIR/res-unique.json"
+VECTOR_ID_OF_COMMON_TEMPLATE_NAME="$BASE_DIR/vector-id-of-common-template-name.json"
+VECTOR_ID_OF_UNIQUE_TEMPLATE_NAME="$BASE_DIR/vector-id-of-unique-template-name.json"
+COMMON_FILE="$BASE_DIR/common.json"
+UNIQUE_FILE="$BASE_DIR/unique.json"
+RESULT_COMMON="$BASE_DIR/res-common.json"
+RESULT_UNIQUE="$BASE_DIR/res-unique.json"
 
-# mkdir -p "$BASE_DIR"
+log_info "Creating directory: "$BASE_DIR""
+mkdir -p "$BASE_DIR"
 
-# # Get only the matches
-# jq '.results[].matches' "$VECTOR_DB_RESPONSE" > "$IN_VECTOR_DB"
+# Get only the matches
+log_info "Get matches array from vector DB response"
+jq '.results[].matches' "$VECTOR_DB_RESPONSE" > "$IN_VECTOR_DB"
 
-# # Get the vectorId and templateName
-# jq -r '[
-#     .[]
-#     | {vectorId: .vectorId, templateName: .metadata.templateName}
-# ]' "$IN_VECTOR_DB" > "$VECTORID_TEMPLATENAME_IN_VECTOR_DB"
+# Get the vectorId and templateName
+log_info "Get the vectorId and templateName from vector DB response"
+jq -r '[
+    .[]
+    | {vectorId: .vectorId, templateName: .metadata.templateName}
+]' "$IN_VECTOR_DB" > "$VECTORID_TEMPLATENAME_IN_VECTOR_DB"
 
-# # Get the templateName present in vector DB
-# jq -r '.[].templateName' "$VECTORID_TEMPLATENAME_IN_VECTOR_DB" > "$TEMPLATENAME_IN_VECTOR_DB"
+# Get the templateName present in vector DB
+log_info "Get the templateName present in vector DB"
+jq -r '.[].templateName' "$VECTORID_TEMPLATENAME_IN_VECTOR_DB" > "$TEMPLATENAME_IN_VECTOR_DB"
 
-# # Get the templateName present in TAO DB
-# jq -r '.[].templateName' "$IN_DB" > "$TEMPLATENAME_IN_DB"
+# Get the templateName present in TAO DB
+log_info "Get the templateName present in TAO DB"
+jq -r '.[].templateName' "$IN_DB" > "$TEMPLATENAME_IN_DB"
 
-# # Find the missing templateName (only present in vector db)
-# comm -13 "$TEMPLATENAME_IN_DB" "$TEMPLATENAME_IN_VECTOR_DB" > "$ONLY_IN_VECTOR_DB_TEMPLATENAME"
+# Find the missing templateName (only present in vector db)
+log_info "Find the missing templateName (only present in vector db)"
+comm -13 "$TEMPLATENAME_IN_DB" "$TEMPLATENAME_IN_VECTOR_DB" > "$ONLY_IN_VECTOR_DB_TEMPLATENAME"
 
-# # Find the common templateName
-# comm -12 "$TEMPLATENAME_IN_DB" "$TEMPLATENAME_IN_VECTOR_DB" > "$COMMON_TEMPLATENAME"
+# Find the common templateName
+log_info "Find the common templateName"
+comm -12 "$TEMPLATENAME_IN_DB" "$TEMPLATENAME_IN_VECTOR_DB" > "$COMMON_TEMPLATENAME"
 
-# jq -R . "$COMMON_TEMPLATENAME" | jq -s . > "$COMMON_FILE"
-# jq -R . "$ONLY_IN_VECTOR_DB_TEMPLATENAME" | jq -s . > "$UNIQUE_FILE"
+log_info "Create common templateName file"
+jq -R . "$COMMON_TEMPLATENAME" | jq -s . > "$COMMON_FILE"
 
-# # Find the vectorId of common templateName
-# jq --slurpfile ids "$COMMON_FILE" '[
-#   .[]
-#   | select(.metadata.templateName | IN($ids[][]))
-#   | .vectorId
-# ]' "$IN_VECTOR_DB" > "$RESULT_COMMON"
+log_info "Create unique templateName file in vector DB"
+jq -R . "$ONLY_IN_VECTOR_DB_TEMPLATENAME" | jq -s . > "$UNIQUE_FILE"
 
-# # Find the vectorId of missing templateName
-# jq --slurpfile ids "$UNIQUE_FILE" '[
-#   .[]
-#   | select(.metadata.templateName | IN($ids[][]))
-#   | .vectorId
-# ]' "$IN_VECTOR_DB" > "$RESULT_UNIQUE"
+# Find the vectorId of common templateName
+log_info "Find the vectorId of common templateName"
+jq --slurpfile ids "$COMMON_FILE" '[
+  .[]
+  | select(.metadata.templateName | IN($ids[][]))
+  | .vectorId
+]' "$IN_VECTOR_DB" > "$RESULT_COMMON"
+
+# Find the vectorId of missing templateName
+log_info "Find the vectorId of missing templateName"
+jq --slurpfile ids "$UNIQUE_FILE" '[
+  .[]
+  | select(.metadata.templateName | IN($ids[][]))
+  | .vectorId
+]' "$IN_VECTOR_DB" > "$RESULT_UNIQUE"
